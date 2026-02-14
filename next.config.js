294e2fcd9b076f5adcd5f1c6b8f72ef87f141dba
manojk38904-gif <manojk38904@gmail.com>
2026-02-14 13:18:29 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,21 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value:
-              "default-src 'self'; script-src 'self' 'unsafe-inline'; img-src 'self' data:; style-src 'self' 'unsafe-inline';",
-          },
-        ],
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
